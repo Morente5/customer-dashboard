d3b004da9d432fa5004de64984bfeda3a0393f8b
Alejandro Morente <morente5@hotmail.es>
2017-06-11 20:09:48 +0200

    New calendar component

--- public/src/app/routes/tasks/tasks.module.ts
+++ public/src/app/routes/tasks/tasks.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
 import { TasksComponent } from './tasks.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		TasksRoutingModule
 	],
-	declarations: [TasksComponent]
+	declarations: [TasksComponent, CalendarComponent]
 })
 export class TasksModule { }
