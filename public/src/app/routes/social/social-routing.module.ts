4a47219ea1fb8c3ea8521c2cbf8c87f4786337bd
Alejandro Morente <morente5@hotmail.es>
2017-06-11 20:01:38 +0200

    Required omponent routing in lazy-loaded modules

--- public/src/app/routes/social/social-routing.module.ts
+++ public/src/app/routes/social/social-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SocialComponent } from './social.component';
+
+const routes: Routes = [
+	{ path: '', component: SocialComponent }
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
--- public/src/app/routes/tasks/tasks-routing.module.ts
+++ public/src/app/routes/tasks/tasks-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TasksComponent } from './tasks.component';
+
+const routes: Routes = [
+	{ path: '', component: TasksComponent }
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
