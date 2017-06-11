d309c3ef88e3643db7e3f58928fbf0048b8132ca
Alejandro Morente <morente5@hotmail.es>
2017-06-11 20:14:14 +0200

    Build an array of days to make a monthly view

--- public/src/app/routes/tasks/calendar/calendar.component.ts
+++ public/src/app/routes/tasks/calendar/calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import 'moment/locale/es';
 
 @Component({
 	selector: 'bmc-calendar',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+	public today: moment.Moment
+	public month: moment.Moment[] = []
+
 	constructor() { }
 
 	ngOnInit() {
+		this.today = moment().startOf('day')
+		this.month = this.getMonth(this.today);
+		console.log(this.month)
+		console.log(this.getFullWeekMonth(this.today))
+	}
+
+	getMonth(day: moment.Moment): moment.Moment[] {
+		let result = []
+
+		let date = day
+			.clone()  // Makes a copy
+			.startOf('month')  // Sets time to start of a month
+
+		while (date.month() === day.month()) {
+			result.push(date.clone());
+			date.add(1, 'day')
+		}
+
+		return result
+	}
+
+	getFullWeekMonth(day: moment.Moment): moment.Moment[] {
+		let result = []
+
+		let date = day
+			.clone()  // Makes a copy
+			.startOf('month')  // Sets time to start of a month
+			.startOf('week')  // Sets time to the first day of the week
+		// console.log(date)
+
+		while (date.month() <= day.month() || !date.isSame(date.clone().startOf('week'))) {
+			result.push(date.clone());
+			date.add(1, 'day')
+		}
+
+		return result
 	}
 
 }
