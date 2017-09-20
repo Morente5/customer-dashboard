d79b1610aec9801ed52e3467ac7a9f007e7d091d
Alejandro Morente <morente5@hotmail.es>
2017-09-20 10:07:16 +0200

    Import Firebase

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -10,6 +10,19 @@ import { ComponentsModule } from 'app/tools/components/components.module';
 // Routing Module
 import { AppRoutingModule } from 'app/app.routing';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+export const firebaseConfig = {
+	apiKey: 'AIzaSyBpzP5Elt_aSv3KB87n_VRVvuJ7ZeXHugM',
+	authDomain: 'dashboard-braun-marketing.firebaseapp.com',
+	databaseURL: 'https://dashboard-braun-marketing.firebaseio.com/',
+	storageBucket: 'your-domain-name.appspot.com',
+	messagingSenderId: '774638353867'
+};
+
 /**
  * The app bootstrapper module
  */
@@ -20,6 +33,9 @@ import { AppRoutingModule } from 'app/app.routing';
 	imports: [
 		AppRoutingModule,
 		BrowserModule,
+		AngularFireModule.initializeApp(firebaseConfig),
+		AngularFireDatabaseModule,
+		AngularFireAuthModule,
 		ComponentsModule,
 	],
 	providers: [
