7521b0b497ac5f18ecd030ee32507d8bf714f301
Alejandro Morente <morente5@hotmail.es>
2017-09-21 09:26:58 +0200

    Styled basic layout

--- public/src/app/tools/components/layout/side-bar/side-bar.component.ts
+++ public/src/app/tools/components/layout/side-bar/side-bar.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-	public clientes = ['cliente 1', 'cliente 2', 'cliente 3', 'cliente 4']
+	public clientes = ['cliente 1 con un nombre larguisimo de la hostia', 'cliente 2', 'cliente 3', 'cliente 4']
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	slug(string: string): string {
+		return string.toString().toLowerCase()
+			.replace(/\s+/g, '-')           // Replace spaces with -
+			.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+			.replace(/\-\-+/g, '-')         // Replace multiple - with single -
+			.replace(/^-+/, '')             // Trim - from start of text
+			.replace(/-+$/, '')
 	}
 
 }
