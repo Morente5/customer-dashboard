99b969afbb480c915a2c9058b9686d59d0b1826e
Alejandro Morente <morente5@hotmail.es>
2017-10-07 14:27:02 +0200

    Renamed top-bar->topbar

--- public/src/app/tools/components/layout/topbar/topbar.component.spec.ts
+++ public/src/app/tools/components/layout/topbar/topbar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TopBarComponent } from './top-bar.component';
+import { TopbarComponent } from './topbar.component';
 
-describe('TopBarComponent', () => {
-	let component: TopBarComponent;
-	let fixture: ComponentFixture<TopBarComponent>;
+describe('TopbarComponent', () => {
+	let component: TopbarComponent;
+	let fixture: ComponentFixture<TopbarComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ TopBarComponent ]
+			declarations: [ TopbarComponent ]
 		})
 		.compileComponents();
 	}));
 
 	beforeEach(() => {
-		fixture = TestBed.createComponent(TopBarComponent);
+		fixture = TestBed.createComponent(TopbarComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
---
+++ public/src/app/tools/components/layout/topbar/topbar.component.ts
@@ -0,0 +1,34 @@
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+
+import { AuthService } from 'app/tools/services/auth/auth.service'
+
+import { Router } from '@angular/router'
+
+@Component({
+	selector: 'bmc-topbar',
+	templateUrl: './topbar.component.html',
+	styleUrls: ['./topbar.component.scss']
+})
+export class TopbarComponent implements OnInit {
+	@Output() onToggleSidebar = new EventEmitter();
+	@Input() user;
+	@Input() projects$;
+
+	constructor(
+		public authService: AuthService
+	) { }
+
+	ngOnInit() {
+	}
+	public toggleSidebar() {
+		this.onToggleSidebar.emit()
+	}
+	// private loginClick(): void {
+	// 	this.authService.login()
+	// }
+	public logoutClick(): void {
+		this.authService.logout()
+	}
+
+}
