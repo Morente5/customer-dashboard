e04504bda18c3eab053f933b4ef1b2e1a8ed441a
Alejandro Morente <morente5@hotmail.es>
2017-10-07 14:25:23 +0200

    Renamed maincontent->main

--- public/src/app/tools/components/layout/main/main.component.spec.ts
+++ public/src/app/tools/components/layout/main/main.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainContentComponent } from './main-content.component';
+import { MainComponent } from './main.component';
 
-describe('MainContentComponent', () => {
-	let component: MainContentComponent;
-	let fixture: ComponentFixture<MainContentComponent>;
+describe('MainComponent', () => {
+	let component: MainComponent;
+	let fixture: ComponentFixture<MainComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ MainContentComponent ]
+			declarations: [ MainComponent ]
 		})
 		.compileComponents();
 	}));
 
 	beforeEach(() => {
-		fixture = TestBed.createComponent(MainContentComponent);
+		fixture = TestBed.createComponent(MainComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
