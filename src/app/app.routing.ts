9d37ad167a0db79f77923dc0d1a6cf9abb6a48b2
Alejandro Morente <morente5@hotmail.es>
2018-03-06 10:35:25 +0100

    Routes Named

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,38 +4,56 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Router Auth Guard
-import { AuthGuard, LoginGuard, AdminGuard, DashboardGuard } from './shared/services/auth/auth-guard.service';
+import { AuthGuard, LoginGuard, AdminGuard, DashboardGuard } from '@bmc-shared/services/auth/auth-guard.service';
 
 const routeBase = './views/';
 const routes: Routes = [
 	{
 		path: '',
+		data: {
+			title: 'Home'
+		},
 		loadChildren: routeBase + 'home/home.module#HomeModule'
 	},
 	{
 		path: 'login',
 		canActivate: [LoginGuard],
+		data: {
+			title: 'Sign In'
+		},
 		loadChildren: routeBase + 'login/login.module#LoginModule'
 	},
 	{
-		path: 'perfil',
+		path: 'profile',
 		canActivate: [AuthGuard],
+		data: {
+			title: 'Profile'
+		},
 		loadChildren: routeBase + 'profile/profile.module#ProfileModule'
 	},
 	{
 		path: 'admin',
 		canActivate: [AdminGuard],
+		data: {
+			title: 'Admin Panel'
+		},
 		loadChildren: routeBase + 'admin/admin.module#AdminModule'
 	},
 	{
 		path: 'access-denied',
+		data: {
+			title: 'Access Denied'
+		},
 		loadChildren: routeBase + 'access-denied/access-denied.module#AccessDeniedModule'
 	},
 	{
 		path: ':projectID',
 		canActivate: [DashboardGuard],
-		loadChildren: routeBase + 'project/project.module#ProjectModule'
-	},
+		data: {
+			title: 'Project Dashboard'
+		},
+		loadChildren: routeBase + 'project-dashboard/project-dashboard.module#ProjectModule'
+	}
 ];
 
 @NgModule({
