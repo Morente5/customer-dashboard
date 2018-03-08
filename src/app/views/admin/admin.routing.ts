380c046faf5841bf1c71f030e44df89b2d6d3ae8
Alejandro Morente <morente5@hotmail.es>
2018-03-08 09:02:56 +0100

    New admin routing deleting empty component

--- src/app/views/admin/admin.routing.ts
+++ src/app/views/admin/admin.routing.ts
@@ -5,43 +5,35 @@ import { AdminComponent } from './admin.component';
 import { AdminUsersComponent } from './admin-users/admin-users.component';
 import { AdminProjectsComponent } from './admin-projects/admin-projects.component';
 import { UserDetailComponent } from './admin-users/user-detail/user-detail.component';
-import { AdminEmptyComponent } from './admin-empty/admin-empty.component';
 import { ProjectDetailComponent } from './admin-projects/project-detail/project-detail.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: AdminComponent,
-		children: [
-			{
-				path: '',
-				component: AdminEmptyComponent,
+	},
+	{
+		path: 'users',
+		component: AdminUsersComponent,
+	},
+	{
+		path: 'users/:userID',
+		component: UserDetailComponent
+	},
+	{
+		path: 'projects',
+		component: AdminProjectsComponent
+	},
+	{
+		path: 'projects/:projectID',
+		component: ProjectDetailComponent
+	},
+	{
+		path: '**',
+		redirectTo: '',
+		pathMatch: 'full'
+	},
 
-			},
-			{
-				path: 'users',
-				component: AdminUsersComponent,
-
-			},
-			{
-				path: 'users/:userID',
-				component: UserDetailComponent
-			},
-			{
-				path: 'projects',
-				component: AdminProjectsComponent
-			},
-			{
-				path: 'projects/:projectID',
-				component: ProjectDetailComponent
-			},
-			{
-				path: '**',
-				redirectTo: '',
-				pathMatch: 'full'
-			},
-		]
-	}
 ];
 
 @NgModule({
