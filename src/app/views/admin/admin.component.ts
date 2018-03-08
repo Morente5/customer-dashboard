0074323a508f8033ed7ef38b949dd3a15a9cffd6
Alejandro Morente <morente5@hotmail.es>
2018-03-08 09:06:01 +0100

    Better data management and styling in admin ad profile views

--- src/app/views/admin/admin.component.ts
+++ src/app/views/admin/admin.component.ts
@@ -12,9 +12,22 @@ import { NbRouteTabsetModule, /*NbActionsModule*/ } from '@nebular/theme';
 })
 export class AdminComponent implements OnInit {
 	tabs = [
-		{ title: 'Users', route: '/admin/users' },
+		{ title: 'Users', route: '/admin/users', data: {title: 'Users'} },
 		{ title: 'Projects', route: '/admin/projects' }
 	];
+	public adminMenu = {
+		'users': {
+			name: 'Users',
+			icon: 'nb-person',
+			color: 'primary'
+		},
+		'projects': {
+			name: 'Projects',
+			icon: 'nb-compose',
+			color: 'danger'
+		},
+	}
+
 	constructor(/*public adminUsersService: AdminUsersService*/) {}
 
 	ngOnInit() {}
