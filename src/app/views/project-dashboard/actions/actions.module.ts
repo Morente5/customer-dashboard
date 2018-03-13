e385bf2193202aa4618094e363db4ae44c7392c1
Alejandro Morente <morente5@hotmail.es>
2018-03-13 11:41:37 +0100

    Deleted unused calendar components

--- src/app/views/project-dashboard/actions/actions.module.ts
+++ src/app/views/project-dashboard/actions/actions.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { SharedModule } from '@bmc-shared/shared.module';
 
 import { ActionsRoutingModule } from './actions.routing';
 
 import { ActionsComponent } from './actions.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { DayComponent } from './calendar/day/day.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		ActionsRoutingModule,
-		ReactiveFormsModule,
 
 		SharedModule
 	],
-	declarations: [ActionsComponent, CalendarComponent, DayComponent]
+	declarations: [ActionsComponent]
 })
 export class ActionsModule { }
