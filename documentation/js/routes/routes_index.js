846231b53d650ea3a75661dbff19e641a66ff32d
Alejandro Morente <morente5@hotmail.es>
2018-03-13 12:05:06 +0100

    Deleted documentation, generate it running `npm run compodoc`

--- documentation/js/routes/routes_index.js
+++
@@ -1,1 +0,0 @@
-var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"name":"routes","filename":"src/app/app.routing.ts","module":"AppRoutingModule","kind":"module"}]}
