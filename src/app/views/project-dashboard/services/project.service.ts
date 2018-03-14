dcbae4e2252d4c3ccb5b4c176f59c652197de92a
Alejandro Morente <morente5@hotmail.es>
2018-03-14 08:54:36 +0100

    Reorganize firestore project document with subcollections

--- src/app/views/project-dashboard/services/project.service.ts
+++ src/app/views/project-dashboard/services/project.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 import { AngularFirestore } from 'angularfire2/firestore';
+import { DocumentSnapshot, DocumentData } from '@firebase/firestore-types';
 
 import { Observable } from 'rxjs/Observable';
 import { map, distinctUntilChanged, switchMap } from 'rxjs/operators'
@@ -32,10 +33,10 @@ export class ProjectService {
 
 	public actionsUrl$(projectID: string): Observable<SafeResourceUrl> {
 		if (projectID) {
-			const path = `actions/${projectID}`
-			return this.afs.doc(path).snapshotChanges().map(project => {
-				if (project && project.payload.data().hasOwnProperty('url')) {
-					return this.sanitize(project.payload.data().url)
+			const path = `projects/${projectID}/actions/data`
+			return this.afs.doc(path).snapshotChanges().map(actionsDocument => {
+				if (actionsDocument.payload.exists && actionsDocument.payload.data().hasOwnProperty('url')) {
+					return this.sanitize(actionsDocument.payload.data().url)
 				} else {
 					return undefined
 				}
@@ -44,7 +45,7 @@ export class ProjectService {
 		return Observable.of(undefined)
 	}
 
-	private urlResponsive$(urlObj$: Observable<any>): Observable<SafeResourceUrl> {
+	private urlResponsive$(urlObj$: Observable<DocumentData>): Observable<SafeResourceUrl> {
 		return this.windowService.windowWidth$.pipe(
 			map(width => {
 				return width <= 768 ? 'mobile' : 'desktop'
@@ -53,20 +54,20 @@ export class ProjectService {
 			switchMap(res => {
 				if (res === 'mobile') {
 					return urlObj$.map(frame => {
-						if (frame && frame.hasOwnProperty('mobile')) {
-							return this.sanitize(frame.mobile)
-						} else if (frame && frame.hasOwnProperty('desktop')) {
-							return this.sanitize(frame.desktop)
+						if (frame && frame.hasOwnProperty('mobileUrl')) {
+							return this.sanitize(frame.mobileUrl)
+						} else if (frame && frame.hasOwnProperty('desktopUrl')) {
+							return this.sanitize(frame.desktopUrl)
 						} else {
 							return undefined
 						}
 					})
 				} else if (res === 'desktop') {
 					return urlObj$.map(frame => {
-						if (frame && frame.hasOwnProperty('desktop')) {
-							return this.sanitize(frame.desktop)
-						} else if (frame && frame.hasOwnProperty('mobile')) {
-							return this.sanitize(frame.mobile)
+						if (frame && frame.hasOwnProperty('desktopUrl')) {
+							return this.sanitize(frame.desktopUrl)
+						} else if (frame && frame.hasOwnProperty('mobileUrl')) {
+							return this.sanitize(frame.mobileUrl)
 						} else {
 							return undefined
 						}
@@ -78,9 +79,9 @@ export class ProjectService {
 
 	public adwordsUrl$(projectID: string): Observable<SafeResourceUrl> {
 		if (projectID) {
-			const path = `ad-words/${projectID}`
-			const urlObj$ = this.afs.doc(path).snapshotChanges().map(project => {
-				return project.payload.data()
+			const path = `projects/${projectID}/ad-words/data`
+			const urlObj$ = this.afs.doc(path).snapshotChanges().map(adwordsDocument => {
+				return adwordsDocument.payload.data()
 			})
 			return this.urlResponsive$(urlObj$)
 		}
@@ -89,9 +90,9 @@ export class ProjectService {
 
 	public analyticsUrl$(projectID: string): Observable<SafeResourceUrl> {
 		if (projectID) {
-			const path = `analytics/${projectID}`
-			const urlObj$ = this.afs.doc(path).snapshotChanges().map(project => {
-				return project.payload.data()
+			const path = `projects/${projectID}/analytics/data`
+			const urlObj$ = this.afs.doc(path).snapshotChanges().map(analyticsDocument => {
+				return analyticsDocument.payload.data()
 			})
 			return this.urlResponsive$(urlObj$)
 		}
