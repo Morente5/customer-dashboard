bea42c06ee433f59eb1798f36aa1f07c158a4947
Alejandro Morente <morente5@hotmail.es>
2018-03-15 13:01:29 +0100

    Oops. Forgot to commit file.

--- src/app/layout/topbar/topbar.component.ts
+++ src/app/layout/topbar/topbar.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import { filter, tap } from 'rxjs/operators';
+
+import { NbSidebarService, NbMenuItem, NbMenuService } from '@nebular/theme';
 
 import { AuthService } from '@bmc-core/services/auth.service';
 import { RouterService } from '@bmc-core/services/router.service';
+import { TitleService } from '@bmc-core/services/title.service';
 
-import { NbSidebarService, NbMenuItem, NbContextMenuDirective } from '@nebular/theme';
-
-import { Router } from '@angular/router';
+import { Project } from '@bmc-core/model/project';
 
 @Component({
 	selector: 'bmc-topbar',
@@ -13,30 +19,66 @@ import { Router } from '@angular/router';
 	styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent implements OnInit {
-	@Input() sidebarState: 'expanded' | 'compacted' | 'collapsed';
+	@Input() sidebarState: 'expanded' | 'compacted' | 'collapsed'
 
-	adminMenu: NbMenuItem[] = [{ title: 'Profile', link: '/profile' }, { title: 'Admin Panel', link: '/admin' }, { title: 'Log out' }];
-	userMenu: NbMenuItem[] = [{ title: 'Profile', link: '/profile' }, { title: 'Log out' }];
+	adminMenu: NbMenuItem[] = [
+		{ title: 'Profile', link: '/profile', icon: 'nb-person' },
+		{ title: 'Admin Panel', link: '/admin', icon: 'nb-gear' },
+		{ title: 'Sign Out', icon: 'nb-close-circled' }
+	]
 
-	title: string;
+	userMenu: NbMenuItem[] = [
+		{ title: 'Profile', link: '/profile', icon: 'nb-person' },
+		{ title: 'Sign Out', icon: 'nb-close-circled' }
+	]
+
+	title: string
+	project: Project
 
 	constructor(
 		private sidebarService: NbSidebarService,
 		public authService: AuthService,
 		public routerService: RouterService,
-		private router: Router
-	) { }
+		private router: Router,
+		private titleService: TitleService,
+		private menuService: NbMenuService
+	) {	}
 
 	ngOnInit() {
-		// this.routerService. .subscribe(pepe => this.title = pepe);
-		this.routerService.routerTitle$.subscribe(title => this.title = title)
+		this.title$.subscribe(title => this.title = title);
+		this.project$.subscribe(project => this.project = project);
+
+		this.menuService.onItemClick().pipe(
+			filter(event => event.item.title === 'Sign Out'),
+			tap(() => this.signOut())
+		).subscribe()
+
 	}
-	public toggleSidebar() {
+
+	public get title$(): Observable<string> {
+		return this.routerService.routerTitle$
+	}
+
+	public get project$(): Observable<Project> {
+		return this.routerService.routerProject$
+	}
+
+	public get renderTitle(): string {
+		if (this.title === 'Project Dashboard' && this.project) {
+			return this.project.name
+		} else if (this.project && this.project.name) {
+			return `${this.title} - ${this.project.name}`
+		} else {
+			return this.title
+		}
+	}
+
+	public toggleSidebar(): void {
 		this.sidebarService.toggle(true);
 	}
 
-	public logoutClick(): void {
-		this.authService.logout();
+	public signOut(): void {
+		this.authService.signOut();
 	}
 
 	public homeClick(): void {
