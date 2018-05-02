7a9a6ffd277b555dfbf0cea1a9f3b3866984979c
Alejandro Morente <morente5@hotmail.es>
2018-05-02 09:42:31 +0200

    Moved passwords model to feature subfolder

--- src/app/views/project-dashboard/passwords/model/passwords.ts
+++ src/app/views/project-dashboard/passwords/model/passwords.ts
@@ -1,26 +1,29 @@
 export interface PasswordGroup {
+	id: string
 	groupName: string
 	groupFields?: PasswordField[]
 }
-export class PasswordGroup implements PasswordGroup { }
+export class PasswordGroup implements PasswordGroup {
+	constructor(args?) {
+		if (args) {
+			Object.assign(this, args)
+		}
+	}
+}
 
 export interface PasswordField {
+	id?: string
 	name: string
 	value?: string
 	isPassword?: boolean
-	visible?: boolean
 }
 
 export class PasswordField implements PasswordField {
 
-	constructor(fields?: PasswordField) {
-		this.visible = false
+	constructor(fields?) {
 		if (fields) {
 			Object.assign(this, fields)
 		}
 	}
 
-	public toggleVisibility(): void {
-		this.visible = !this.visible
-	}
 }
