d4424f8e510e64f30d8a7ff96db52627760bc89f
Alejandro Morente <morente5@hotmail.es>
2018-05-04 23:32:04 +0200

    Updated to Angular v6 release Moved some config files Added @angular/pwa, moved service worker and manifest files

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('customer-dashboard App', () => {
+describe('workspace-project App', () => {
 	let page: AppPage;
 
 	beforeEach(() => {
@@ -9,6 +9,6 @@ describe('customer-dashboard App', () => {
 
 	it('should display welcome message', () => {
 		page.navigateTo();
-		expect(page.getParagraphText()).toEqual('Welcome to bmc!');
+		expect(page.getParagraphText()).toEqual('Welcome to app!');
 	});
 });
