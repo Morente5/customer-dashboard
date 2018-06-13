b72a10d3705c594d8dbd11f9c2cb29707185d55a
Alejandro Morente <morente5@hotmail.es>
2018-06-13 09:23:24 +0200

    Show correct title on topbar and window title

--- src/app/core/services/title.service.ts
+++ src/app/core/services/title.service.ts
@@ -2,9 +2,45 @@ import { Injectable } from '@angular/core';
 
 import { Title } from '@angular/platform-browser';
 
+import { RouterService } from '@bmc-core/services/router.service';
+import { Observable } from 'rxjs';
+import { Project } from '@bmc-core/model/project';
+
 @Injectable()
 export class TitleService {
 
-	constructor() { }
+	title = 'CustomerDashboard'
+	project: Project
+
+	constructor(
+		private routerService: RouterService,
+		private browserTitle: Title
+	) {
+		this.title$.subscribe(title => this.title = title);
+		this.project$.subscribe(project => this.project = project);
+
+		this.renderTitle()
+		this.routerService.routerProject$.subscribe(() => {
+			this.browserTitle.setTitle(`${this.renderTitle()} - Customer Dashboard`)
+		})
+	}
+
+	public get title$(): Observable<string> {
+		return this.routerService.routerTitle$
+	}
+
+	public get project$(): Observable<Project> {
+		return this.routerService.routerProject$
+	}
+
+	public renderTitle(): string {
+		if (this.title === 'Project Dashboard' && this.project) {
+			return this.project.name
+		} else if (this.project && this.project.name) {
+			return `${this.title} - ${this.project.name}`
+		} else {
+			return this.title
+		}
+	}
 
 }
--- src/app/layout/topbar/topbar.component.ts
+++ src/app/layout/topbar/topbar.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Router } from '@angular/router';
 
-import { Observable } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
 
 import { NbSidebarService, NbMenuItem, NbMenuService } from '@nebular/theme';
 
 import { AuthService } from '@bmc-core/services/auth.service';
-import { RouterService } from '@bmc-core/services/router.service';
 import { TitleService } from '@bmc-core/services/title.service';
 
 import { Project } from '@bmc-core/model/project';
@@ -38,16 +36,12 @@ export class TopbarComponent implements OnInit {
 	constructor(
 		private sidebarService: NbSidebarService,
 		public authService: AuthService,
-		public routerService: RouterService,
 		private router: Router,
 		private titleService: TitleService,
 		private menuService: NbMenuService
 	) { }
 
 	ngOnInit() {
-		this.title$.subscribe(title => this.title = title);
-		this.project$.subscribe(project => this.project = project);
-
 		this.menuService.onItemClick().pipe(
 			filter(event => event.item.title === 'Sign Out'),
 			tap(() => this.signOut())
@@ -55,22 +49,8 @@ export class TopbarComponent implements OnInit {
 
 	}
 
-	public get title$(): Observable<string> {
-		return this.routerService.routerTitle$
-	}
-
-	public get project$(): Observable<Project> {
-		return this.routerService.routerProject$
-	}
-
 	public get renderTitle(): string {
-		if (this.title === 'Project Dashboard' && this.project) {
-			return this.project.name
-		} else if (this.project && this.project.name) {
-			return `${this.title} - ${this.project.name}`
-		} else {
-			return this.title
-		}
+		return this.titleService.renderTitle()
 	}
 
 	public toggleSidebar(): void {
